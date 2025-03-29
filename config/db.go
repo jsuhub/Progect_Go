c2197fd5b033d20afdfda5d59e12c932deb8d7de
threejinZhou <zx1186340002@gmail.com>
2025-03-29 19:42:30 +0800

    第一次测试

--- config/db.go
+++ config/db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=gagamel  password=gagamel dbname=gagameldb port=5432 sslmode=disable"
+	dsn := "host=gargamel_postgres user=gargamel  password=gargamel dbname=gargameldb port=5432 sslmode=disable"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	log.Println("数据库初始化成功", DB)
