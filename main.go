d8e4ec8b9d7415aa02a919dc45df11fab0de8113
threejinZhou <zx1186340002@gmail.com>
2025-03-29 20:27:11 +0800

    第三次测试

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"Progect/config"
-	"Progect/log"
+	"log"
+	"os"
+
 	"Progect/router"
 )
 
+func init() {
+	// 设置全局日志输出到文件
+	f, err := os.OpenFile("/var/log/myapp.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("无法打开日志文件: %v", err)
+	}
+	log.SetOutput(f)
+}
+
 func main() {
-	log.Start()
+	log.Println("main: 启动程序")
 	config.InitDB()
 	r := router.SetRouter() // 带 Logger 和 Recovery 的引擎
 	r.Run("0.0.0.0:8888")   // 默认监听 0.0.0.0:8888
